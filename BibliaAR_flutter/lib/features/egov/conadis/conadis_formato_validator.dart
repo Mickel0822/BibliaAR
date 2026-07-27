9d479650560006da0e39450c39a847fe54aea4bf
Kevin Guanoluisa <kevinguanoluisa2004@gmail.com>
2026-07-27 09:00:00 -0500

    BIAR-60: crear estructura base de la validaciÃ³n del formato del nÃºmero de certificado CONADIS

---
+++ BibliaAR_flutter/lib/features/egov/conadis/conadis_formato_validator.dart
@@ -0,0 +1,8 @@
+﻿// kguanoluisa, Estructura base del validador de formato del numero de certificado CONADIS CON-YYYY-NNNNNN, variable v_patronConadis, 2026-07-27
+class ConadisFormatoValidator {
+  static final RegExp vPatronConadis = RegExp(r'^CON-\d{4}-\d{6}$');
+
+  static bool esFormatoValido(String numero) {
+    return vPatronConadis.hasMatch(numero.trim().toUpperCase());
+  }
+}
