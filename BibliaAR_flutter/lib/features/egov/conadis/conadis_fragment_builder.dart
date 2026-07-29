b75a32d1dd6bbb9fb1ad2816346e2bd7319bf1e3
Cristian Bayas <cbayas0410@gmail.com>
2026-07-28 22:30:00 -0500

    BIAR-63: ajustar UI/UX

--- BibliaAR_flutter/lib/features/egov/conadis/conadis_fragment_builder.dart
+++ BibliaAR_flutter/lib/features/egov/conadis/conadis_fragment_builder.dart
@@ -1,24 +1,26 @@
 import 'package:biblia_ar_flutter/data/models/certificado_conadis.dart';
 import 'package:biblia_ar_flutter/data/models/fragmento_narrativo.dart';
 
-// Cristian Bayas, Fragmento resultado CONADIS, cbayas0410@gmail.com, BIAR-63
+// Cristian Bayas, Fragmento resultado CONADIS con tipo y porcentaje, cbayas0410@gmail.com, BIAR-63
 class ConadisFragmentBuilder {
   static List<FragmentoNarrativo> construirFragmentosResultado({
     required CertificadoConadis certificado,
   }) {
+    final pictogramaTipo = certificado.tipoDiscapacidad;
+
     return [
       FragmentoNarrativo(
         id: 1,
         titulo: 'Certificado encontrado',
         descripcion:
-            '${certificado.tipoDiscapacidad}: ${certificado.porcentaje}% certificado.',
+            '${certificado.etiquetaTipoDiscapacidad}: ${certificado.porcentaje}% certificado.',
         ilustracionAsset: '',
         videoLseAsset: '',
         audioAsset: '',
         duracionMs: 5000,
-        pictogramas: const ['certificado'],
+        pictogramas: ['certificado', pictogramaTipo],
         textoSubtitulo:
-            'Porcentaje certificado: ${certificado.porcentaje}%.',
+            'Tipo: ${certificado.etiquetaTipoDiscapacidad}. Porcentaje certificado: ${certificado.porcentaje}%.',
       ),
     ];
   }
